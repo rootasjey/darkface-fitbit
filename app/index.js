539aa79b3b7fde9db79ce4deafab9bbbce47904d
Jeremie Corpinot <jeremiecorpinot@outlook.com>
2018-01-14 22:07:37 +0100

    Update clock face, add icons, activities

--- app/index.js
+++ app/index.js
@@ -3,12 +3,22 @@ import document from "document";
 import { display } from "display";
 import { HeartRateSensor } from "heart-rate";
 
+import { goals } from "user-activity";
+import { today } from "user-activity";
+
 import * as util from "../common/utils";
 
 // Get a handle on the <text> element
-let _clockFace = document.getElementById("clockFace");
+let _root = document.getElementById("root");
+let _clockDigits1 = document.getElementById("clockDigits1");
+let _clockDigits2 = document.getElementById("clockDigits2");
 let _date = document.getElementById("date");
-let _heartRate = document.getElementById("heartrate");
+let _heartRate = document.getElementById("heartRate");
+
+let _mainDisplay = document.getElementById("mainDisplay");
+let _secondDisplay = document.getElementById("secondDisplay");
+
+let _activityIndex = 0;
 
 // Update the clock every minute
 clock.granularity = "minutes";
@@ -27,7 +37,8 @@ const _days = {
   2: 'TUE',
   3: 'WED',
   4: 'THU',
-  5: 'FRI'
+  5: 'FRI',
+  6: 'SAT'
 };
 
 const _months = {
@@ -56,19 +67,19 @@ function updateClock() {
   let hours = today.getHours();
   let mins = util.zeroPad(today.getMinutes());
 
-  _clockFace.text = `${hours}:${mins}`;
+  //_clockFace.text = `${hours}:${mins}`;
+  _clockDigits1.text = `${hours}`;
+  _clockDigits2.text = `${mins}`;
 }
 
 function updateHeartRateSensor() {
   // Begin monitoring the sensor
   _hrm.start();
-  console.log("starting heart")
 }
 
 function stopHearRateSensor() {
   // Stop monitoring the sensor
   _hrm.stop();
-  console.log("stoping heart")
 }
 
 // Update the clock every tick event
@@ -84,4 +95,83 @@ display.addEventListener('change', function() {
   }
 });
 
+_root.onclick = (e) => {
+  showNextActivity();
+}
+
+function updateArcMeter(name) {
+  let distance = document.getElementById(name);
+  let distanceArc = document.getElementById(name + 'Arc');
+  
+  let todaySteps = (today.local[name] || 0);  
+  let percentSteps = todaySteps * 360 / (goals[name] || 1);
+  
+  distanceArc.sweepAngle = percentSteps;
+  distance.text = todaySteps;
+}
+
+function showNextActivity() {
+  _activityIndex = (_activityIndex + 1) % 6;
+  
+  switch (_activityIndex) {
+    case 0:
+      showHeartrate();
+      break;
+    case 1:
+      showSteps();
+      break;
+    case 2:
+      showElevationGain();
+      break;
+    case 3:
+      showDistance();
+      break;
+    case 4:
+      showActiveMinutes();
+      break;
+    case 5:
+      showCalories();
+    default:
+      break;
+  }
+}
+
+function showHeartrate() {
+  hideAndShow('caloriesContainer', 'heartRateContainer');
+}
+
+function showSteps() {
+  hideAndShow('heartRateContainer', 'stepsContainer');
+  updateArcMeter('steps');
+}
+
+function showElevationGain() {
+  hideAndShow('stepsContainer', 'elevationGainContainer');  
+  updateArcMeter('elevationGain');
+}
+
+function showDistance() {
+  hideAndShow('elevationGainContainer', 'distanceContainer');  
+  updateArcMeter('distance');
+}
+
+function showActiveMinutes() {
+  hideAndShow('distanceContainer', 'activeMinutesContainer');
+  updateArcMeter('activeMinutes');
+}
+
+function showCalories() {
+  hideAndShow('activeMinutesContainer', 'caloriesContainer');
+  updateArcMeter('calories');
+}
+
+function hideAndShow(elementToHide, elementToShow) {
+  let elementToHide = document.getElementById(elementToHide);
+  let elementToShow = document.getElementById(elementToShow);
+  
+  elementToHide.style.visibility = 'hidden';
+  elementToShow.style.visibility = 'visible';
+}
+
 updateDate();
+updateHeartRateSensor();
